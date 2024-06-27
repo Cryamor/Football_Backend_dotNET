d398e6205104c4e905b394cee90193c58c3e93c8
ZMC <mczhangtj@outlook.com>
2024-06-27 19:43:41 +0800

    delete AI trash

--- Player/Data/impl/PlayerRepository.cs
+++ Player/Data/impl/PlayerRepository.cs
@@ -19,264 +19,39 @@ namespace Player.Data
             _connectionString = configuration.GetConnectionString("GamesDbConnection");
         }
 
-        public void UpdatePlayer(PlayerSimpleInfo player)
+        public long Count(string league)
         {
-            using (var connection = new MySqlConnection(_connectionString))
-            {
-                connection.Open();
-                var query = @"
-                    INSERT INTO player (id, name, league, firstName, lastName, age, height, weight, country, photo, birth, updateTime)
-                    VALUES (@id, @name, @league, @firstName, @lastName, @age, @height, @weight, @country, @photo, @birth, NOW())
-                    ON DUPLICATE KEY UPDATE
-                    name = VALUES(name),
-                    league = VALUES(league),
-                    firstName = VALUES(firstName),
-                    lastName = VALUES(lastName),
-                    age = VALUES(age),
-                    height = VALUES(height),
-                    weight = VALUES(weight),
-                    country = VALUES(country),
-                    photo = VALUES(photo),
-                    birth = VALUES(birth),
-                    updateTime = NOW();";
-
-                using (var command = new MySqlCommand(query, connection))
-                {
-                    command.Parameters.AddWithValue("@id", player.Id);
-                    command.Parameters.AddWithValue("@name", player.Name);
-                    command.Parameters.AddWithValue("@league", player.League);
-                    command.Parameters.AddWithValue("@firstName", player.FirstName);
-                    command.Parameters.AddWithValue("@lastName", player.LastName);
-                    command.Parameters.AddWithValue("@age", player.Age);
-                    command.Parameters.AddWithValue("@height", player.Height);
-                    command.Parameters.AddWithValue("@weight", player.Weight);
-                    command.Parameters.AddWithValue("@country", player.Country);
-                    command.Parameters.AddWithValue("@photo", player.Photo);
-                    command.Parameters.AddWithValue("@birth", player.Birth);
-                    command.ExecuteNonQuery();
-                }
-            }
-        }
-
-        public void UpdatePlayerMore(PlayerDetailInfo player)
-        {
-            using (var connection = new MySqlConnection(_connectionString))
-            {
-                connection.Open();
-                var query = @"
-                    INSERT INTO player (id, name, firstName, lastName, age, height, weight, country, photo, birth, updateTime)
-                    VALUES (@id, @name, @firstName, @lastName, @age, @height, @weight, @country, @photo, @birth, NOW())
-                    ON DUPLICATE KEY UPDATE
-                    name = VALUES(name),
-                    firstName = VALUES(firstName),
-                    lastName = VALUES(lastName),
-                    age = VALUES(age),
-                    height = VALUES(height),
-                    weight = VALUES(weight),
-                    country = VALUES(country),
-                    photo = VALUES(photo),
-                    birth = VALUES(birth),
-                    updateTime = NOW();";
-
-                using (var command = new MySqlCommand(query, connection))
-                {
-                    command.Parameters.AddWithValue("@id", player.Id);
-                    command.Parameters.AddWithValue("@name", player.Name);
-                    command.Parameters.AddWithValue("@firstName", player.FirstName);
-                    command.Parameters.AddWithValue("@lastName", player.LastName);
-                    command.Parameters.AddWithValue("@age", player.Age);
-                    command.Parameters.AddWithValue("@height", player.Height);
-                    command.Parameters.AddWithValue("@weight", player.Weight);
-                    command.Parameters.AddWithValue("@country", player.Country);
-                    command.Parameters.AddWithValue("@photo", player.Photo);
-                    command.Parameters.AddWithValue("@birth", player.Birth);
-                    command.ExecuteNonQuery();
-                }
-            }
-        }
-
-        public void UpdatePlayerDetail(int playerId, PlayerDetailInfo.SeasonInfo detailInfo)
-        {
-            using (var connection = new MySqlConnection(_connectionString))
-            {
-                connection.Open();
-                var query = @"
-                    INSERT INTO seasonInfo (playerid, seasonNum, teamName, leagueName, leagueLogo, position, goals, assists, passes, tackles, yellow, red, updateTime)
-                    VALUES (@playerId, @seasonNum, @teamName, @leagueName, @leagueLogo, @position, @goals, @assists, @passes, @tackles, @yellow, @red, NOW())
-                    ON DUPLICATE KEY UPDATE
-                    playerid = VALUES(playerid),
-                    seasonNum = VALUES(seasonNum),
-                    teamName = VALUES(teamName),
-                    leagueName = VALUES(leagueName),
-                    leagueLogo = VALUES(leagueLogo),
-                    position = VALUES(position),
-                    goals = VALUES(goals),
-                    assists = VALUES(assists),
-                    passes = VALUES(passes),
-                    tackles = VALUES(tackles),
-                    yellow = VALUES(yellow),
-                    red = VALUES(red),
-                    updateTime = NOW();";
-
-                using (var command = new MySqlCommand(query, connection))
-                {
-                    command.Parameters.AddWithValue("@playerId", playerId);
-                    command.Parameters.AddWithValue("@seasonNum", detailInfo.SeasonNum);
-                    command.Parameters.AddWithValue("@teamName", detailInfo.TeamName);
-                    command.Parameters.AddWithValue("@leagueName", detailInfo.LeagueName);
-                    command.Parameters.AddWithValue("@leagueLogo", detailInfo.LeagueLogo);
-                    command.Parameters.AddWithValue("@position", detailInfo.Position);
-                    command.Parameters.AddWithValue("@goals", detailInfo.Goals);
-                    command.Parameters.AddWithValue("@assists", detailInfo.Assists);
-                    command.Parameters.AddWithValue("@passes", detailInfo.Passes);
-                    command.Parameters.AddWithValue("@tackles", detailInfo.Tackles);
-                    command.Parameters.AddWithValue("@yellow", detailInfo.Yellow);
-                    command.Parameters.AddWithValue("@red", detailInfo.Red);
-                    command.ExecuteNonQuery();
-                }
-            }
-        }
-
-        public List<PlayerDetailInfo.SeasonInfo> GetSeasonInfo(int playerId)
-        {
-            var seasonInfos = new List<PlayerDetailInfo.SeasonInfo>();
-
-            using (var connection = new MySqlConnection(_connectionString))
-            {
-                connection.Open();
-                var query = @"
-                    SELECT * FROM seasonInfo
-                    WHERE playerid = @playerId";
-
-                using (var command = new MySqlCommand(query, connection))
-                {
-                    command.Parameters.AddWithValue("@playerId", playerId);
-
-                    using (var reader = command.ExecuteReader())
-                    {
-                        while (reader.Read())
-                        {
-                            var seasonInfo = new PlayerDetailInfo.SeasonInfo
-                            {
-                                PlayerId = reader.GetInt32("playerid"),
-                                SeasonNum = reader.GetInt32("seasonNum"),
-                                TeamName = reader.GetString("teamName"),
-                                LeagueName = reader.GetString("leagueName"),
-                                LeagueLogo = reader.GetString("leagueLogo"),
-                                Position = reader.GetString("position"),
-                                Goals = reader.GetInt32("goals"),
-                                Assists = reader.GetInt32("assists"),
-                                Passes = reader.GetInt32("passes"),
-                                Tackles = reader.GetInt32("tackles"),
-                                Yellow = reader.GetInt32("yellow"),
-                                Red = reader.GetInt32("red"),
-                                UpdateTime = reader.GetDateTime("updateTime")
-                            };
-                            seasonInfos.Add(seasonInfo);
-                        }
-                    }
-                }
-            }
-            return seasonInfos;
+            throw new NotImplementedException();
         }
 
         public PlayerSimpleInfo GetPlayer(int playerId)
         {
-            using (var connection = new MySqlConnection(_connectionString))
-            {
-                connection.Open();
-                var query = @"
-                    SELECT * FROM player
-                    WHERE id = @playerId";
-
-                using (var command = new MySqlCommand(query, connection))
-                {
-                    command.Parameters.AddWithValue("@playerId", playerId);
-
-                    using (var reader = command.ExecuteReader())
-                    {
-                        if (reader.Read())
-                        {
-                            return new PlayerSimpleInfo
-                            {
-                                Id = reader.GetInt32("id"),
-                                Name = reader.GetString("name"),
-                                League = reader.GetString("league"),
-                                FirstName = reader.GetString("firstName"),
-                                LastName = reader.GetString("lastName"),
-                                Age = reader.GetInt32("age"),
-                                Height = reader.GetString("height"),
-                                Weight = reader.GetString("weight"),
-                                Country = reader.GetString("country"),
-                                Photo = reader.GetString("photo"),
-                                Birth = reader.GetString("birth"),
-                            };
-                        }
-                    }
-                }
-            }
-            return null;
+            throw new NotImplementedException();
         }
 
-        public long Count(string league)
+        public List<PlayerDetailInfo.SeasonInfo> GetSeasonInfo(int playerId)
         {
-            using (var connection = new MySqlConnection(_connectionString))
-            {
-                connection.Open();
-                var query = @"
-                    SELECT count(*) FROM player
-                    WHERE (@league IS NULL OR @league = '' OR @league = '全部赛事' OR league LIKE CONCAT('%', @league, '%'))";
-
-                using (var command = new MySqlCommand(query, connection))
-                {
-                    command.Parameters.AddWithValue("@league", string.IsNullOrEmpty(league) || league == "全部赛事" ? (object)DBNull.Value : league);
-                    return (long)command.ExecuteScalar();
-                }
-            }
+            throw new NotImplementedException();
         }
 
         public List<PlayerSimpleInfo> SelectPlayers(int start, int size, string league)
         {
-            var players = new List<PlayerSimpleInfo>();
+            throw new NotImplementedException();
+        }
 
-            using (var connection = new MySqlConnection(_connectionString))
-            {
-                connection.Open();
-                var query = @"
-                SELECT id, name, league, firstName, lastName, age, height, weight, country, photo
-                FROM player
-                WHERE (@league IS NULL OR @league = '' OR @league = '全部赛事' OR league LIKE CONCAT('%', @league, '%'))
-                LIMIT @start, @size";
+        public void UpdatePlayer(PlayerSimpleInfo player)
+        {
+            throw new NotImplementedException();
+        }
 
-                using (var command = new MySqlCommand(query, connection))
-                {
-                    command.Parameters.AddWithValue("@start", start);
-                    command.Parameters.AddWithValue("@size", size);
-                    command.Parameters.AddWithValue("@league", string.IsNullOrEmpty(league) || league == "全部赛事" ? (object)DBNull.Value : league);
+        public void UpdatePlayerDetail(int playerId, PlayerDetailInfo.SeasonInfo detailInfo)
+        {
+            throw new NotImplementedException();
+        }
 
-                    using (var reader = command.ExecuteReader())
-                    {
-                        while (reader.Read())
-                        {
-                            var player = new PlayerSimpleInfo
-                            {
-                                Id = reader.GetInt32("id"),
-                                Name = reader.GetString("name"),
-                                League = reader.GetString("league"),
-                                FirstName = reader.GetString("firstName"),
-                                LastName = reader.GetString("lastName"),
-                                Age = reader.GetInt32("age"),
-                                Height = reader.GetString("height"),
-                                Weight = reader.GetString("weight"),
-                                Country = reader.GetString("country"),
-                                Photo = reader.GetString("photo")
-                            };
-                            players.Add(player);
-                        }
-                    }
-                }
-            }
-            return players;
+        public void UpdatePlayerMore(PlayerDetailInfo player)
+        {
+            throw new NotImplementedException();
         }
     }
 }
--- Player/Data/impl/TeamRepository.cs
+++ Player/Data/impl/TeamRepository.cs
@@ -14,230 +14,39 @@ namespace Player.Data
             _connectionString = configuration.GetConnectionString("GamesDbConnection");
         }
 
-        private MySqlConnection GetConnection()
-        {
-            return new MySqlConnection(_connectionString);
-        }
-
-        public void UpdateTeam(TeamSimpleInfo team)
-        {
-            using (var conn = GetConnection())
-            {
-                conn.Open();
-                string sql = @"
-                    INSERT INTO team (id, name, country, logo, founded, updateTime)
-                    VALUES (@Id, @Name, @Country, @Logo, @Founded, NOW())
-                    ON DUPLICATE KEY UPDATE
-                    name = VALUES(name),
-                    country = VALUES(country),
-                    logo = VALUES(logo),
-                    founded = VALUES(founded),
-                    updateTime = NOW();
-                ";
-
-                using (var cmd = new MySqlCommand(sql, conn))
-                {
-                    cmd.Parameters.AddWithValue("@Id", team.Id);
-                    cmd.Parameters.AddWithValue("@Name", team.Name);
-                    cmd.Parameters.AddWithValue("@Country", team.Country);
-                    cmd.Parameters.AddWithValue("@Logo", team.Logo);
-                    cmd.Parameters.AddWithValue("@Founded", team.Founded);
-                    cmd.ExecuteNonQuery();
-                }
-            }
-        }
-
-        public void UpdateTeamDetail(TeamDetailInfo team)
-        {
-            using (var conn = GetConnection())
-            {
-                conn.Open();
-                string sql = @"
-                    INSERT INTO team (id, name, country, logo, founded, venueName, venueAddress, venueCity, venueImage, venueCapacity, updateTime)
-                    VALUES (@Id, @Name, @Country, @Logo, @Founded, @VenueName, @VenueAddress, @VenueCity, @VenueImage, @VenueCapacity, NOW())
-                    ON DUPLICATE KEY UPDATE
-                    name = VALUES(name),
-                    country = VALUES(country),
-                    logo = VALUES(logo),
-                    founded = VALUES(founded),
-                    venueName = VALUES(venueName),
-                    venueAddress = VALUES(venueAddress),
-                    venueCity = VALUES(venueCity),
-                    venueImage = VALUES(venueImage),
-                    venueCapacity = VALUES(venueCapacity),
-                    updateTime = NOW();
-                ";
-
-                using (var cmd = new MySqlCommand(sql, conn))
-                {
-                    cmd.Parameters.AddWithValue("@Id", team.Id);
-                    cmd.Parameters.AddWithValue("@Name", team.Name);
-                    cmd.Parameters.AddWithValue("@Country", team.Country);
-                    cmd.Parameters.AddWithValue("@Logo", team.Logo);
-                    cmd.Parameters.AddWithValue("@Founded", team.Founded);
-                    cmd.Parameters.AddWithValue("@VenueName", team.VenueName);
-                    cmd.Parameters.AddWithValue("@VenueAddress", team.VenueAddress);
-                    cmd.Parameters.AddWithValue("@VenueCity", team.VenueCity);
-                    cmd.Parameters.AddWithValue("@VenueImage", team.VenueImage);
-                    cmd.Parameters.AddWithValue("@VenueCapacity", team.VenueCapacity);
-                    cmd.ExecuteNonQuery();
-                }
-            }
-        }
-
-        public void UpdateMember(TeamDetailInfo.PlayerInfo player, int teamid)
-        {
-            using (var conn = GetConnection())
-            {
-                conn.Open();
-                string sql = @"
-                    INSERT INTO player (id, name, age, number, position, photo, teamid, updateTime)
-                    VALUES (@Id, @Name, @Age, @Number, @Position, @Photo, @TeamId, NOW())
-                    ON DUPLICATE KEY UPDATE
-                    id = VALUES(id),
-                    name = VALUES(name),
-                    age = VALUES(age),
-                    number = VALUES(number),
-                    position = VALUES(position),
-                    photo = VALUES(photo),
-                    teamid = VALUES(teamid),
-                    updateTime = NOW();
-                ";
-
-                using (var cmd = new MySqlCommand(sql, conn))
-                {
-                    cmd.Parameters.AddWithValue("@Id", player.Id);
-                    cmd.Parameters.AddWithValue("@Name", player.Name);
-                    cmd.Parameters.AddWithValue("@Age", player.Age);
-                    cmd.Parameters.AddWithValue("@Number", player.Number);
-                    cmd.Parameters.AddWithValue("@Position", player.Position);
-                    cmd.Parameters.AddWithValue("@Photo", player.Photo);
-                    cmd.Parameters.AddWithValue("@TeamId", teamid);
-                    cmd.ExecuteNonQuery();
-                }
-            }
-        }
-
         public long Count()
         {
-            using (var conn = GetConnection())
-            {
-                conn.Open();
-                string sql = "SELECT COUNT(*) FROM team";
-                using (var cmd = new MySqlCommand(sql, conn))
-                {
-                    return Convert.ToInt64(cmd.ExecuteScalar());
-                }
-            }
-        }
-
-        public List<TeamSimpleInfo> SelectTeams(int start, int size)
-        {
-            using (var conn = GetConnection())
-            {
-                conn.Open();
-                string sql = @"
-                    SELECT id, name, country, logo, founded
-                    FROM team
-                    LIMIT @Start, @Size
-                ";
-
-                using (var cmd = new MySqlCommand(sql, conn))
-                {
-                    cmd.Parameters.AddWithValue("@Start", start);
-                    cmd.Parameters.AddWithValue("@Size", size);
-
-                    using (var reader = cmd.ExecuteReader())
-                    {
-                        List<TeamSimpleInfo> teams = new List<TeamSimpleInfo>();
-                        while (reader.Read())
-                        {
-                            TeamSimpleInfo team = new TeamSimpleInfo();
-                            team.Id = reader.GetInt32("id");
-                            team.Name = reader.GetString("name");
-                            team.Country = reader.GetString("country");
-                            team.Logo = reader.GetString("logo");
-                            team.Founded = reader.GetString("founded");
-                            teams.Add(team);
-                        }
-                        return teams;
-                    }
-                }
-            }
-        }
-
-        public TeamDetailInfo GetTeam(int id)
-        {
-            using (var conn = GetConnection())
-            {
-                conn.Open();
-                string sql = @"
-                    SELECT id, name, country, logo, founded, venueName, venueAddress, venueCity, venueImage, venueCapacity
-                    FROM team
-                    WHERE id = @Id
-                ";
-
-                using (var cmd = new MySqlCommand(sql, conn))
-                {
-                    cmd.Parameters.AddWithValue("@Id", id);
-
-                    using (var reader = cmd.ExecuteReader())
-                    {
-                        if (reader.Read())
-                        {
-                            TeamDetailInfo team = new TeamDetailInfo();
-                            team.Id = reader.GetInt32("id");
-                            team.Name = reader.GetString("name");
-                            team.Country = reader.GetString("country");
-                            team.Logo = reader.GetString("logo");
-                            team.Founded = reader.GetString("founded");
-                            team.VenueName = reader.GetString("venueName");
-                            team.VenueAddress = reader.GetString("venueAddress");
-                            team.VenueCity = reader.GetString("venueCity");
-                            team.VenueImage = reader.GetString("venueImage");
-                            team.VenueCapacity = reader.GetInt32("venueCapacity");
-                            return team;
-                        }
-                        return null;
-                    }
-                }
-            }
+            throw new NotImplementedException();
         }
 
         public List<TeamDetailInfo.PlayerInfo> GetMember(int id)
         {
-            using (var conn = GetConnection())
-            {
-                conn.Open();
-                string sql = @"
-                    SELECT id, name, age, number, position, photo
-                    FROM player
-                    WHERE teamid = @Id
-                ";
+            throw new NotImplementedException();
+        }
 
-                using (var cmd = new MySqlCommand(sql, conn))
-                {
-                    cmd.Parameters.AddWithValue("@Id", id);
+        public TeamDetailInfo GetTeam(int id)
+        {
+            throw new NotImplementedException();
+        }
 
-                    using (var reader = cmd.ExecuteReader())
-                    {
-                        List<TeamDetailInfo.PlayerInfo> members = new List<TeamDetailInfo.PlayerInfo>();
-                        while (reader.Read())
-                        {
-                            TeamDetailInfo.PlayerInfo player = new TeamDetailInfo.PlayerInfo();
-                            player.Id = reader.GetInt32("id");
-                            player.Name = reader.GetString("name");
-                            player.Age = reader.GetInt32("age");
-                            player.Number = reader.GetInt32("number");
-                            player.Position = reader.GetString("position");
-                            player.Photo = reader.GetString("photo");
-                            members.Add(player);
-                        }
-                        return members;
-                    }
-                }
-            }
+        public List<TeamSimpleInfo> SelectTeams(int start, int size)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateMember(TeamDetailInfo.PlayerInfo player, int teamid)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateTeam(TeamSimpleInfo team)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateTeamDetail(TeamDetailInfo team)
+        {
+            throw new NotImplementedException();
         }
     }
 }
-
